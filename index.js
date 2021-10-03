6392d01dd25ef2ba6c0aa927f030ba4098de2818
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-04 01:56:58 +0700

    testAPI func

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { languageApi } from './api/language-api.js';
+import { submissionApi } from './api/submission-api.js';
 import { Header } from './components/header.js';
 import { HomePage } from './components/home-page.js';
 import { Login } from './components/login.js';
@@ -23,6 +25,18 @@ firebase.auth().onAuthStateChanged(function (user) {
     $container.appendChild(new Header().$container);
     $container.appendChild($hello);
     setScreen($container);
+
+    (async function testAPI() {
+      const data = {
+        source_code: '#include <stdio.h>\n\nint main(void) {\n printf("hello");\n  return 0;\n}',
+        language_id: 54,
+        stdin: 'world',
+      };
+
+      // const submission = await submissionApi.create(data);
+      // const status = await submissionApi.get(submission.token);
+      // console.log(status);
+    })();
   } else {
     const homePage = new HomePage();
     setScreen(homePage.$container);
