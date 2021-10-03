6f9573b4384b0438dbdba3b0a73c685534edc445
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-03 18:40:16 +0700

    add home-page

---
+++ components/home-page.js
@@ -0,0 +1,22 @@
+import { Footer } from './footer.js';
+import { Header } from './header.js';
+
+class HomePage {
+  $container = document.createElement('div');
+  $header = new Header();
+  $main = document.createElement('div');
+  $footer = new Footer();
+
+  constructor() {
+    {
+      // MAIN
+    }
+    {
+      // FOOTER
+    }
+    this.$container.appendChild(this.$header.$container);
+    this.$container.appendChild(this.$main);
+    this.$container.appendChild(this.$footer.$container);
+  }
+}
+export { HomePage };
