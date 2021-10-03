c7f6267a2691fc579f57a03701e9b8f0c42c7d84
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-03 18:39:20 +0700

    config firebase

---
+++ index.js
@@ -0,0 +1,30 @@
+import { HomePage } from './components/home-page.js';
+import { Login } from './components/login.js';
+
+const app = document.getElementById('app');
+
+function setScreen($container) {
+  app.innerHTML = '';
+  app.appendChild($container);
+}
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    const username = user.username;
+    const email = user.email;
+    const uid = user.uid;
+    console.log(username, email, uid);
+
+    const $container = document.createElement('div');
+    const $hello = document.createElement('h2');
+    $hello.textContent = `Hello ${email}`;
+    $container.appendChild($hello);
+    setScreen($container);
+  } else {
+    const homePage = new HomePage();
+    setScreen(homePage.$container);
+  }
+});
+
+export { setScreen };
