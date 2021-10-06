583ea69f7949f50a6b8a1ce617e7e3374b9c6d24
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-06 15:47:49 +0700

    show language submit

--- components/submission/submission-item.js
+++ components/submission/submission-item.js
@@ -1,16 +1,26 @@
 class SubmissionItem {
   $container = document.createElement('li');
   $title = document.createElement('h5');
+  $language = document.createElement('div');
   $status = document.createElement('div');
   $score = document.createElement('div');
   $compileStatus = document.createElement('div');
   $compileDetails = document.createElement('div');
 
   constructor(data) {
+    const LANGUAGES = {
+      py: 'Python',
+      c: 'C',
+      cpp: 'C++',
+      pas: 'Pascal',
+      java: 'Java',
+    };
     this.$container.appendChild(this.$title);
+    this.$container.appendChild(this.$language);
     this.$container.appendChild(this.$status);
 
     this.$title.textContent = `${data.displayName || data.userId} submitted ${data.problemId}`;
+    this.$language.textContent = `Language: ${LANGUAGES[data.language] || data.language}`;
     this.$status.textContent = `Status: ${data.status}`;
     this.$score.textContent = `Scored: ${data.score || 0}`;
 
