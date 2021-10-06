ef1ef993173fa01fcc817acd748569487217aabe
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-06 14:11:48 +0700

    getSubmission func

--- submission/get-submission.js
+++ submission/get-submission.js
@@ -1,14 +1,14 @@
-function getSubmission(id) {
+function getSubmission(id, callbackFn) {
   if (!id) return;
+  if (!callbackFn) callbackFn = (data) => console.log('Submission data:', data);
   firebase
     .firestore()
     .collection('submissions')
     .doc(id)
     .get()
     .then((doc) => {
-      if (doc.exists) {
-        console.log('Submission data:', doc.data());
-      } else console.log('No such submission has id', id);
+      if (doc.exists) callbackFn(doc.data());
+      else console.log('No such submission has id', id);
     })
     .catch((error) => console.log('Error getting submission:', error));
 }
