b2575e9dff984621175d3b4949fe773c6c46bfaa
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-06 14:56:55 +0700

    add Submisstions component

---
+++ components/submission/submission-list.js
@@ -0,0 +1,21 @@
+import { SubmissionItem } from './submission-item.js';
+
+class SubmissionList {
+  $container = document.createElement('div');
+  $title = document.createElement('h4');
+  $submissionList = document.createElement('ul');
+
+  constructor(title = 'Submissions:', submissionList = []) {
+    this.$container.appendChild(this.$title);
+    this.$container.appendChild(this.$submissionList);
+
+    this.$title.textContent = title;
+    submissionList.forEach((item) => {
+      console.log(item);
+      const $li = new SubmissionItem(item).$container;
+      this.$submissionList.appendChild($li);
+    });
+  }
+}
+
+export { SubmissionList };
