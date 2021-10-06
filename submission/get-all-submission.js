978a1051f1a2a684202712edace8ba5802d1ec9a
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-06 15:19:24 +0700

    getAllSubmission order by 'sentAt' field

--- submission/get-all-submission.js
+++ submission/get-all-submission.js
@@ -3,6 +3,7 @@ const db = firebase.firestore();
 function getAllSubmission(callbackFn) {
   if (!callbackFn) callbackFn = (data) => console.log(data);
   db.collection('submissions')
+    .orderBy('sentAt')
     .get()
     .then((querySnapshot) => {
       const tempDoc = [];
