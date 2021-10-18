cb0312eac68e23c76b9b4b1df3ac63b9d64b69da
lmhdev <84726648+lmhdev@users.noreply.github.com>
2021-10-18 23:03:01 +0700

    Add submission-list page

--- components/submission/submission-list.js
+++ components/submission/submission-list.js
@@ -1,19 +1,42 @@
-import { SubmissionItem } from './submission-item.js';
+import { Header } from '../header.js';
+import { SideNav } from '../side-nav.js';
+import { SubmissionRow } from './submission-row.js';
 
 class SubmissionList {
   $container = document.createElement('div');
-  $title = document.createElement('h4');
-  $submissionList = document.createElement('ul');
+  $nav = new SideNav();
+  $main = document.createElement('div');
 
-  constructor(title = 'Submissions:', submissionList = []) {
-    this.$container.appendChild(this.$title);
-    this.$container.appendChild(this.$submissionList);
+  $header = new Header();
+  $contentWrapper = document.createElement('div');
+  $table = document.createElement('table');
+  $thead = document.createElement('thead');
 
-    this.$title.textContent = title;
-    submissionList.forEach((item) => {
-      const $li = new SubmissionItem(item).$container;
-      this.$submissionList.appendChild($li);
-    });
+  $submissionRows = new SubmissionRow();
+
+  constructor() {
+    this.$container.appendChild(this.$nav.$container);
+    this.$container.appendChild(this.$main);
+
+    this.$main.appendChild(this.$header.$container);
+    this.$main.appendChild(this.$contentWrapper);
+    this.$main.style.marginLeft = '80px';
+
+    this.$contentWrapper.classList.add('exercise-container', 'm-5');
+    this.$contentWrapper.appendChild(this.$table);
+
+    this.$table.classList.add('table', 'table-success', 'table-striped');
+    this.$table.appendChild(this.$thead);
+    this.$table.appendChild(this.$submissionRows.$container);
+    this.$thead.innerHTML = `<tr>
+    <th scope="col">User</th>
+    <th scope="col">Execise Name</th>
+    <th scope="col">Score</th>
+    <th scope="col">Difficulty Level</th>
+    <th scope="col">Status</th>
+  </tr>`;
+
+    this.$submissionRows.getData();
   }
 }
 
---
+++ components/submission/submission-row.js
@@ -0,0 +1,26 @@
+class SubmissionRow {
+  $container = document.createElement('tbody');
+  constructor() {}
+
+  getData = () => {
+    firebase
+      .firestore()
+      .collection('submissions')
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const newRow = document.createElement('tr');
+          newRow.innerHTML = `<tr>
+          <th scope="col">${doc.data().displayName}</th>
+          <th scope="col">${doc.data().problemId}</th>
+          <th scope="col">${doc.data().language}</th>
+          <th scope="col">${doc.data().score}</th>
+          <th scope="col">${doc.data().status}</th>
+        </tr>`;
+          this.$container.appendChild(newRow);
+        });
+      });
+  };
+}
+
+export { SubmissionRow };
