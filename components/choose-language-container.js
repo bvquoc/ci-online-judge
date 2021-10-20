864000b39f87d686294685341665af47ba7e7d0e
bvquoc <quocpro10.02.2003@gmail.com>
2021-10-20 10:19:39 +0700

    edit languages on home-page

--- components/choose-language-container.js
+++ components/choose-language-container.js
@@ -8,15 +8,16 @@ class ChooseLanguage {
     this.$container.appendChild(this.$languageList);
     this.$container.classList.add('choose-language-container');
 
-    this.$txtTitle.innerHTML = 'Please pick a language!';
+    this.$txtTitle.innerHTML = 'Programming languages';
     this.$languageList.innerHTML = `<a href=""
     ><img
       src="https://img.icons8.com/material-outlined/24/000000/program.png"
     />
     Pascal</a
     >
-    <a href=""><img src="./img/python.png" alt="" />Python</a>
-    <a href=""><img src="./img/c.png" alt="" /> C/C++</a>
+    <a href=""><img src="./img/c.png" alt="" />C</a>
+    <a href=""><img src="./img/c++.png" alt="" />C++</a>
+    <a href=""><img src="./img/java.png" alt="" />Java</a>
     <a href=""
         ><img
         src="./img/javascript.png"
@@ -24,7 +25,16 @@ class ChooseLanguage {
         class="coming-soon"
         />JavaScript
         <p>(Coming Soon!)</p></a
-    >`;
+    >
+    <a href=""
+        ><img
+        src="./img/python.png"
+        alt=""
+        class="coming-soon"
+        />Python
+        <p>(Coming Soon!)</p></a
+    >
+    `;
 
     this.$languageList.classList.add('language-list');
   }
